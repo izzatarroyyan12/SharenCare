c0f333e546d9f0763c8cc35692a5b43459010f7b
mailbau <simailpipiw@gmail.com>
2023-09-21 16:54:33 +0700

    update donor and receiver class

--- SharenCare_cs/Classes/Donor.cs
+++ SharenCare_cs/Classes/Donor.cs
@@ -8,5 +8,19 @@ namespace SharenCare_cs.Classes
 {
     class Donor
     {
+        private int donorId;
+        private List<Donation> donatedItem;
+
+        public int donor_Id { get { return donorId; } }
+        public List<Donation> donated_Item { get { return donatedItem; } set { donatedItem = value; } }
+
+        public void donateItem()
+        {
+            //logic
+        }
+        public void editItem()
+        {
+            //logic
+        }
     }
 }
--- SharenCare_cs/Classes/Receiver.cs
+++ SharenCare_cs/Classes/Receiver.cs
@@ -8,5 +8,19 @@ namespace SharenCare_cs.Classes
 {
     class Receiver
     {
+        private int receiverId;
+        private List<Donation> receivedItems;
+
+        public int receiver_Id { get { return receiverId; } }
+        public List<Donation> received_Items { get { return receivedItems; } set { receivedItems = value; } }
+
+        public void takeItem()
+        {
+            //logic
+        }
+        public void searchItem()
+        {
+            //logic
+        }
     }
 }
