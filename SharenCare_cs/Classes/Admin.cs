84553808b7f37fc0f6324c8f33b4641c1dd50461
putrasitorus456 <putrasitorus456@gmail.com>
2023-09-21 23:59:44 +0700

    Add Admin and User class, update Receiver and Donor class.

--- SharenCare_cs/Classes/Admin.cs
+++ SharenCare_cs/Classes/Admin.cs
@@ -8,5 +8,61 @@ namespace SharenCare_cs.Classes
 {
     class Admin
     {
+        private int id;
+        private string username;
+        private string email;
+        private string password;
+
+        public int id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public Admin(int id, string username, string email, string password)
+        {
+            this.id = id;
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+
+        public bool Login(string username, string password)
+        {
+            // Implementasi logika login admin
+        }
+
+        public void ManageUsers()
+        {
+            // Implementasi logika manajemen pengguna (users)
+        }
+
+        public void ManageItems()
+        {
+            // Implementasi logika manajemen item
+        }
+
+        public void ManageDonations()
+        {
+            // Implementasi logika manajemen donasi
+        }
     }
 }
