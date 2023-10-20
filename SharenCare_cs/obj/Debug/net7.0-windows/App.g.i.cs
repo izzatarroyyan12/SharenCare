bf0547a414343c1195193e0de30517c703bd1927
mailbau <simailpipiw@gmail.com>
2023-10-20 14:08:17 +0700

    delete

--- SharenCare_cs/obj/Debug/net7.0-windows/App.g.i.cs
+++ SharenCare_cs/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace SharenCare_cs {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace SharenCare_cs {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public static void Main() {
             SharenCare_cs.App app = new SharenCare_cs.App();
             app.InitializeComponent();
