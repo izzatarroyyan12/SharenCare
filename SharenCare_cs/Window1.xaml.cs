608c5a2464654e2d11ecf8da54d3292c2be9c731
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2023-10-26 11:36:15 +0700

    Menambahkan UI

--- SharenCare_cs/Window1.xaml.cs
+++ SharenCare_cs/Window1.xaml.cs
@@ -14,14 +14,25 @@ using System.Windows.Shapes;
 
 namespace SharenCare_cs
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public Window1()
         {
             InitializeComponent();
+            // Menambahkan handler acara untuk label "Register here!" jika label ada dalam file XAML
+            if (RegisterLabel != null)
+            {
+                RegisterLabel.MouseLeftButtonDown += RegisterLabel_Click;
+            }
+        }
+
+        private void RegisterLabel_Click(object sender, MouseButtonEventArgs e)
+        {
+            // Membuka jendela baru "Window2"
+            Window2 window2 = new Window2();
+            window2.Show();
+            this.Close(); // Menutup jendela saat berpindah ke Window2
         }
     }
 }
+
